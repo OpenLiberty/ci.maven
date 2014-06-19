08bcfc9a8b30edf3c96c9d38ee28dadf1646655b
Jarek Gawor <jgawor@gmail.com>
2014-06-19 10:50:38 -0400

    #24: 'run-server' goal

---
+++ liberty-maven-plugin/src/main/java/net/wasdev/wlp/maven/plugins/server/RunServerMojo.java
@@ -0,0 +1,55 @@
+/**
+ * (C) Copyright IBM Corporation 2014.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.wasdev.wlp.maven.plugins.server;
+
+import java.text.MessageFormat;
+
+import net.wasdev.wlp.ant.ServerTask;
+
+/**
+ * Start a liberty server
+ * 
+ * @goal run-server
+ */
+public class RunServerMojo extends StartDebugMojoSupport {
+
+    /**
+     * Clean all cached information on server start up.
+     * 
+     * @parameter expression="${clean}" default-value="false"
+     */
+    protected boolean clean;
+
+    @Override
+    protected void doExecute() throws Exception {
+        if (skip) {
+            return;
+        }
+        if (isInstall) {
+            installServerAssembly();
+        } else {
+            log.info(MessageFormat.format(messages.getString("info.install.type.preexisting"), ""));
+            checkServerHomeExists();
+        }
+
+        ServerTask serverTask = initializeJava();
+        copyConfigFiles();
+        serverTask.setClean(clean);
+        serverTask.setOperation("run");       
+        serverTask.execute();
+    }
+
+}
