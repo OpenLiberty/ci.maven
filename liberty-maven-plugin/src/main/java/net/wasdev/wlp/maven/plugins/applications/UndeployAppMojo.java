ccd6581b51d4a3bff68d058879df44a0281d2933
Alexis Resendiz Andrade <alexis.resendiz.andrade@outlook.com>
2015-04-23 15:57:38 -0500

    Support for includes and excludes in the undeploy goal
    README.md changes:
    - Updated deploy documentation to add the appArtifact parameter and
    examples.
    - Updated undeploy documentation to add patternSet parameter and
    examples.
    
    MvnMessages.properties changes:
    - Added a new message to use it when the undeploy is done by a pattern
    set.
    
    UndeployAppMojo changes:
    - Organized imports.
    - Updated mojo documentation.
    - Added patternSet parameter.
    - Added support for the undeploy by pattern sets.

--- liberty-maven-plugin/src/main/java/net/wasdev/wlp/maven/plugins/applications/UndeployAppMojo.java
+++ liberty-maven-plugin/src/main/java/net/wasdev/wlp/maven/plugins/applications/UndeployAppMojo.java
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright IBM Corporation 2014.
+ * (C) Copyright IBM Corporation 2014, 2015.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,89 +19,129 @@ import java.io.File;
 import java.io.IOException;
 import java.text.MessageFormat;
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.codehaus.mojo.pluginsupport.util.ArtifactItem;
-
 import net.wasdev.wlp.ant.UndeployTask;
 import net.wasdev.wlp.maven.plugins.BasicSupport;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.tools.ant.types.PatternSet;
+import org.codehaus.mojo.pluginsupport.util.ArtifactItem;
+
 /**
- * Undeploy application from liberty server
- * 
+ * Undeploy application from liberty server. If no parameters have been defined
+ * the mojo will undeploy all applications from the server.
+ *
  * @goal undeploy
- * 
- * 
  */
 public class UndeployAppMojo extends BasicSupport {
+    
     /**
-     * A file name which points to a specific module's jar | war | ear | eba | zip
-     * archive.
-     * 
+     * A file name which points to a specific module's jar | war | ear | eba |
+     * zip archive.
+     *
      * @parameter expression="${appArchive}"
      * @optional
      */
     protected String appArchive = null;
-
+    
     /**
-     * Maven coordinates of an application to undeploy. This is best listed as a dependency,
-     * in which case the version can be omitted.
-     * 
+     * Maven coordinates of an application to undeploy. This is best listed as a
+     * dependency, in which case the version can be omitted.
+     *
      * @parameter
      */
     protected ArtifactItem appArtifact;
-
+    
+    /**
+     * A set of patterns to include or exclude from the undeploy operation. If
+     * appArchive or appArtifact has been defined then this parameter will be
+     * ignored.
+     *
+     * @parameter
+     */
+    private PatternSet patternSet;
+    
     /**
      * Timeout to verify undeploy successfully, in seconds.
-     * 
+     *
      * @parameter expression="${timeout}" default-value="40"
      */
     protected int timeout = 40;
-
+    
+    /*
+     * (non-Javadoc)
+     * @see org.codehaus.mojo.pluginsupport.MojoSupport#doExecute()
+     */
     @Override
     protected void doExecute() throws Exception {
         if (skip) {
             return;
         }
+        
         checkServerHomeExists();
         checkServerDirectoryExists();
         
-        if (appArchive != null && appArtifact != null) {
-            throw new MojoExecutionException(messages.getString("error.app.set.twice"));
+        UndeployTask undeployTask = (UndeployTask) ant
+                .createTask("antlib:net/wasdev/wlp/ant:undeploy");
+        
+        if (undeployTask == null) {
+            throw new NullPointerException("Undeploy task not found");
         }
-
-        if (appArtifact != null) {
-            Artifact artifact = getArtifact(appArtifact);
-            appArchive = artifact.getFile().getName();
-            log.info(MessageFormat.format(messages.getString("info.variable.set"), "artifact based application", appArtifact));
-        } else if (appArchive != null) {
-            File file = new File(appArchive);
-            if (file.exists()) {
-                appArchive = file.getName();
+        
+        if (appArchive != null || appArtifact != null) {
+            
+            if (appArchive != null && appArtifact != null) {
+                throw new MojoExecutionException(
+                        messages.getString("error.app.set.twice"));
             }
-            log.info(MessageFormat.format(messages.getString("info.variable.set"), "non-artifact based application", appArchive));
+            
+            if (appArtifact != null) {
+                Artifact artifact = getArtifact(appArtifact);
+                appArchive = artifact.getFile().getName();
+                
+                log.info(MessageFormat.format(
+                        messages.getString("info.variable.set"),
+                        "artifact based application", appArtifact));
+                
+            } else if (appArchive != null) {
+                File file = new File(appArchive);
+                if (file.exists()) {
+                    appArchive = file.getName();
+                }
+                
+                log.info(MessageFormat.format(
+                        messages.getString("info.variable.set"),
+                        "non-artifact based application", appArchive));
+                
+            }
+            
+            File destFile = new File(serverDirectory, "dropins/" + appArchive);
+            if (destFile == null || !destFile.exists()
+                    || destFile.isDirectory()) {
+                throw new IOException(MessageFormat.format(
+                        messages.getString("error.undeploy.app.noexist"),
+                        destFile.getCanonicalPath()));
+            }
+            
+            undeployTask.setFile(appArchive);
+            log.info(MessageFormat.format(
+                    messages.getString("info.undeploy.app"),
+                    destFile.getCanonicalPath()));
+            
+        } else if (patternSet != null && patternSet.hasPatterns(ant.getAnt())) {
+            log.info(messages.getString("info.undeploy.patternset"));
+            undeployTask.addPatternset(patternSet);
         } else {
-            throw new MojoExecutionException(MessageFormat.format(messages.getString("error.undeploy.app.set.validate"), ""));
+            log.info(messages.getString("info.undeploy.all"));
         }
-
-        File destFile = new File(serverDirectory, "dropins/" + appArchive);
-        if (destFile == null || !destFile.exists() || destFile.isDirectory()) {
-            throw new IOException(MessageFormat.format(messages.getString("error.undeploy.app.noexist"), destFile.getCanonicalPath()));
-        }
-
-        log.info(MessageFormat.format(messages.getString("info.undeploy.app"), destFile.getCanonicalPath()));
-        UndeployTask undeployTask = (UndeployTask) ant.createTask("antlib:net/wasdev/wlp/ant:undeploy");
-        if (undeployTask == null)
-            throw new NullPointerException("server task not found");
-
+        
         undeployTask.setInstallDir(installDirectory);
         undeployTask.setServerName(serverName);
         undeployTask.setUserDir(userDirectory);
         undeployTask.setOutputDir(outputDirectory);
-        undeployTask.setFile(appArchive);
+        
         // Convert from seconds to milliseconds
-        undeployTask.setTimeout(Long.toString(timeout*1000));
+        undeployTask.setTimeout(Long.toString(timeout * 1000));
         undeployTask.execute();
-
     }
 }
