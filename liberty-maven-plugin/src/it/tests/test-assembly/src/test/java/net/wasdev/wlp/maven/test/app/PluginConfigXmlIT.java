31bc2ff126a1f827e484658c0956060314fccfcd
Patrick Tiu <tiu@ca.ibm.com>
2017-05-03 10:58:38 -0400

    set applicationFilename only when install-apps for project or all (#201)

---
+++ liberty-maven-plugin/src/it/tests/test-assembly/src/test/java/net/wasdev/wlp/maven/test/app/PluginConfigXmlIT.java
@@ -0,0 +1,62 @@
+package net.wasdev.wlp.maven.test.app;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+/**
+ * 
+ * Web application test case
+ * 
+ */
+
+public class PluginConfigXmlIT {
+
+	public final String CONFIG_XML = "target/liberty-plugin-config.xml";
+
+    @Test
+    public void testConfigPropFileExist() throws Exception {
+        File f = new File(CONFIG_XML);
+        Assert.assertTrue(f.getCanonicalFile() + " doesn't exist", f.exists());
+    }
+    
+    @Test
+    public void testXmlElements() throws Exception {
+    	File in = new File(CONFIG_XML);
+        FileInputStream input = new FileInputStream(in);
+        
+        // get input XML Document 
+        DocumentBuilderFactory inputBuilderFactory = DocumentBuilderFactory.newInstance();
+        inputBuilderFactory.setIgnoringComments(true);
+        inputBuilderFactory.setCoalescing(true);
+        inputBuilderFactory.setIgnoringElementContentWhitespace(true);
+        inputBuilderFactory.setValidating(false);
+        DocumentBuilder inputBuilder = inputBuilderFactory.newDocumentBuilder();
+        Document inputDoc=inputBuilder.parse(input);
+        
+        // parse input XML Document
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        String expression = "/liberty-plugin-config/applicationFilename";
+        NodeList nodes = (NodeList) xPath.compile(expression).evaluate(inputDoc, XPathConstants.NODESET);
+        Assert.assertEquals("Number of <applicationFilename/> element ==>", 0, nodes.getLength());
+    }
+    
+    @Test
+    public void testApplicationFileExist() throws Exception {
+        File f = new File("target/liberty/usr/servers/test/dropins/test-eba.eba");
+        Assert.assertTrue(f.getCanonicalFile() + " doesn't exist", f.exists());
+        f = new File("target/liberty/usr/servers/test/dropins/test-war.war");
+        Assert.assertTrue(f.getCanonicalFile() + " doesn't exist", f.exists());
+    }
+}
