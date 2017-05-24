d864bbd7fda859e608b643405cdca1ed09259fdc
Patrick Tiu <tiu@ca.ibm.com>
2017-05-24 14:17:00 -0400

    update webapp archetype default values

--- liberty-archetype-webapp/src/main/resources/archetype-resources/src/test/java/it/EndpointIT.java
+++ liberty-archetype-webapp/src/main/resources/archetype-resources/src/test/java/it/EndpointIT.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-package ${groupId}.it;
+package ${package}.it;
 
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
