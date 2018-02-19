1e09bf8bc5c448bd0693166a77ae27f5c5be192b
Charles Tian <ctian@us.ibm.com>
2018-02-19 16:10:58 -0600

    Fixed appveyor settings.xml format

--- appveyor.ps1
+++ appveyor.ps1
@@ -2,7 +2,12 @@ $mavenConfig = '<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
                   http://maven.apache.org/xsd/settings-1.0.0.xsd">
-                  
+<profiles>
+    <profile>
+      <id>appveyor</id>
+      <activation>
+        <activeByDefault>false</activeByDefault>
+      </activation>
       <repositories>
         <repository>
             <id>sonatype-nexus-snapshots</id>
@@ -16,7 +21,8 @@ $mavenConfig = '<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
             </releases>
         </repository>
       </repositories>
-
+    </profile>
+  </profiles>
 </settings>'
 
 New-Item "$env:USERPROFILE\.m2" -ItemType Directory -Force | Out-Null
