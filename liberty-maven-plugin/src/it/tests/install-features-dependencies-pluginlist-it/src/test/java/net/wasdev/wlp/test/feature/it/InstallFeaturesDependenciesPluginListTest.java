34e6875b6dad67cb31f777afb697da20d888ef8e
Eric Lau <ericglau@ca.ibm.com>
2018-05-17 15:46:06 -0400

    Add integration tests for installing features from Maven repo

--- liberty-maven-plugin/src/it/tests/install-features-dependencies-pluginlist-it/src/test/java/net/wasdev/wlp/test/feature/it/InstallFeaturesDependenciesPluginListTest.java
+++ liberty-maven-plugin/src/it/tests/install-features-dependencies-pluginlist-it/src/test/java/net/wasdev/wlp/test/feature/it/InstallFeaturesDependenciesPluginListTest.java
@@ -21,20 +21,32 @@ import java.io.FilenameFilter;
 import static junit.framework.Assert.*;
 import org.junit.Test;
 
-public class InstallFeatureTest {
-    
+public class InstallFeaturesDependenciesPluginListTest {
+
     @Test
     public void testNumberOfFeatures() throws Exception {
-    	    File dir = new File("liberty/wlp/lib/features");
-        
-    	    File[] features = dir.listFiles(new FilenameFilter() {
-    	        public boolean accept(File dir, String name) {
-    	            return name.toLowerCase().endsWith(".mf");
-    	        }
-    	    });
-    	    
-        assertEquals("Number of installed features", 8, features.length);        
-    }
-    
-}
+        File dir = new File("liberty/wlp/lib/features");
 
+        File[] features = dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".mf");
+            }
+        });
+
+        assertEquals("Number of installed features", 9, features.length);
+        assertContains(features, "a-1.0");
+        assertContains(features, "b-1.0");
+    }
+
+    private void assertContains(File[] files, String feature) {
+        boolean found = false;
+        for (File file : files) {
+            if (file.getName().endsWith("." + feature + ".mf")) {
+                found = true;
+                break;
+            }
+        }
+        assertTrue("Feature " + feature + " was not installed", found);
+    }
+
+}
---
+++ liberty-maven-plugin/src/it/tests/install-features-pluginlist-it/src/test/java/net/wasdev/wlp/test/feature/it/InstallFeaturesPluginListTest.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * (c) Copyright IBM Corporation 2018.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package net.wasdev.wlp.test.feature.it;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+import static junit.framework.Assert.*;
+import org.junit.Test;
+
+public class InstallFeaturesPluginListTest {
+
+    @Test
+    public void testNumberOfFeatures() throws Exception {
+        File dir = new File("liberty/wlp/lib/features");
+
+        File[] features = dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".mf");
+            }
+        });
+
+        assertEquals("Number of installed features", 8, features.length);
+        assertContains(features, "b-1.0");
+    }
+
+    private void assertContains(File[] files, String feature) {
+        boolean found = false;
+        for (File file : files) {
+            if (file.getName().endsWith("." + feature + ".mf")) {
+                found = true;
+                break;
+            }
+        }
+        assertTrue("Feature " + feature + " was not installed", found);
+    }
+
+}
