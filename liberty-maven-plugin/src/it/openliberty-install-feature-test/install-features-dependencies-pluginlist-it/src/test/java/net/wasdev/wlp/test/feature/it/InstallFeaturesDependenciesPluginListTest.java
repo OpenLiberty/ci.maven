20184e6bc85c4e822e528e87d040cf055016068e
Eric Lau <ericglau@ca.ibm.com>
2018-06-12 16:56:40 -0400

    Refactor tests into multi module, update OL build

---
+++ liberty-maven-plugin/src/it/openliberty-install-feature-test/install-features-dependencies-pluginlist-it/src/test/java/net/wasdev/wlp/test/feature/it/InstallFeaturesDependenciesPluginListTest.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * (c) Copyright IBM Corporation 2018.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package net.wasdev.wlp.test.feature.it;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+import static junit.framework.Assert.*;
+import org.junit.Test;
+
+public class InstallFeaturesDependenciesPluginListTest extends BaseInstallFeature {
+
+    @Test
+    public void testNumberOfFeatures() throws Exception {
+        assertEquals("Number of installed features", 9, features.length);
+        assertContains("a-1.0");
+        assertContains("b-1.0");
+    }
+
+}
