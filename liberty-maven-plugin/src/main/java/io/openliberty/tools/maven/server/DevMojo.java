01929880855234ebc6471d3f1561ee526c99c4e5
Trevor Crawford <tcrawfo6@uwo.ca>
2020-03-03 18:14:07 -0500

    Add bootstrapPropertiesFile to be watched

--- liberty-maven-plugin/src/main/java/io/openliberty/tools/maven/server/DevMojo.java
+++ liberty-maven-plugin/src/main/java/io/openliberty/tools/maven/server/DevMojo.java
@@ -752,7 +752,7 @@ public class DevMojo extends StartDebugMojoSupport {
         // which is where the server.xml is located if a specific serverXmlFile
         // configuration parameter is not specified.
         try {
-            util.watchFiles(pom, outputDirectory, testOutputDirectory, executor, artifactPaths, serverXmlFile);
+            util.watchFiles(pom, outputDirectory, testOutputDirectory, executor, artifactPaths, serverXmlFile, bootstrapPropertiesFile);
         } catch (PluginScenarioException e) {
             if (e.getMessage() != null) {
                 // a proper message is included in the exception if the server has been stopped by another process
