2a11b8352b15cfa97ddb4d7dad8dea8088fe5271
Trevor Crawford <tcrawfo6@uwo.ca>
2020-04-16 12:43:07 -0400

    Removed polling and pollingInterval parameters

--- liberty-maven-plugin/src/main/java/io/openliberty/tools/maven/server/DevMojo.java
+++ liberty-maven-plugin/src/main/java/io/openliberty/tools/maven/server/DevMojo.java
@@ -141,18 +141,6 @@ public class DevMojo extends StartDebugMojoSupport {
     protected boolean clean;
 
     /**
-     * Poll for file changes instead of using file system notifications.
-     */
-    @Parameter(property = "polling", defaultValue = "false")
-    protected boolean polling;
-
-    /**
-     * Polling interval in milliseconds.
-     */
-    @Parameter(property = "pollingInterval", defaultValue = "100")
-    private long pollingInterval;
-
-    /**
      * The directory for source files.
      */
     @Parameter(readonly = true, required = true, defaultValue = " ${project.build.sourceDirectory}")
@@ -186,7 +174,7 @@ public class DevMojo extends StartDebugMojoSupport {
                 List<File> resourceDirs) throws IOException {
             super(serverDirectory, sourceDirectory, testSourceDirectory, configDirectory, resourceDirs, hotTests,
                     skipTests, skipUTs, skipITs, project.getArtifactId(), serverStartTimeout, verifyTimeout, verifyTimeout,
-                    ((long) (compileWait * 1000L)), libertyDebug, false, false, polling, pollingInterval);
+                    ((long) (compileWait * 1000L)), libertyDebug, false, false);
 
             ServerFeature servUtil = getServerFeatureUtil();
             this.existingFeatures = servUtil.getServerFeatures(serverDirectory);
