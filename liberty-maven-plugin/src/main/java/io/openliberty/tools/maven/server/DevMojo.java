d3a28d271defba162a1b2ac984a47bc70bda1f0e
Eric Lau <ericglau@ca.ibm.com>
2020-06-25 12:14:17 -0400

    Address review comments

--- liberty-maven-plugin/src/main/java/io/openliberty/tools/maven/server/DevMojo.java
+++ liberty-maven-plugin/src/main/java/io/openliberty/tools/maven/server/DevMojo.java
@@ -102,7 +102,7 @@ public class DevMojo extends StartDebugMojoSupport {
     private int libertyDebugPort;
 
     @Parameter(property = "container", defaultValue = "false")
-    protected boolean container;
+    private boolean container;
 
     /**
      * Time in seconds to wait before processing Java changes and deletions.
@@ -186,6 +186,19 @@ public class DevMojo extends StartDebugMojoSupport {
      */
     @Parameter(property = "dockerRunOpts")
     private String dockerRunOpts;
+
+    /**
+     * Set the container option.
+     * 
+     * @param container whether dev mode should use a container
+     */
+    protected void setContainer(boolean container) {
+        // set container variable for DevMojo
+        this.container = container;
+        
+        // set project property for use in DeployMojoSupport
+        project.getProperties().setProperty("container", Boolean.toString(container));
+    }
 
     private class DevMojoUtil extends DevUtil {
 
@@ -685,9 +698,7 @@ public class DevMojo extends StartDebugMojoSupport {
 
         if (dockerfile != null) {
             if (dockerfile.exists()) {
-                container = true;
-                // set project property for use in DeployMojoSupport
-                project.getProperties().setProperty("container", "true");
+                setContainer(true);
             }
             else {
                 throw new MojoExecutionException("The file " + dockerfile + " used for dev mode option dockerfile does not exist."
--- liberty-maven-plugin/src/main/java/io/openliberty/tools/maven/server/StartDebugMojoSupport.java
+++ liberty-maven-plugin/src/main/java/io/openliberty/tools/maven/server/StartDebugMojoSupport.java
@@ -72,8 +72,6 @@ public class StartDebugMojoSupport extends BasicSupport {
     private static final Pattern pattern = Pattern.compile(LIBERTY_CONFIG_MAVEN_PROPS); 
 
     protected final String PLUGIN_VARIABLE_CONFIG_XML = "configDropins/overrides/liberty-plugin-variable-config.xml";
-    protected final String PROJECT_ROOT_NAME = "io.openliberty.tools.projectRoot";
-    protected final String PROJECT_ROOT_TARGET_LIBS = "target/libs";
 
     protected Map<String,String> bootstrapMavenProps = new HashMap<String,String>();  
     protected Map<String,String> envMavenProps = new HashMap<String,String>();  
