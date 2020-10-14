bc7644d9d190cb9ba0328f22f045f531c9f5e103
jjvilleg <jjvilleg@us.ibm.com>
2020-10-14 15:51:00 -0500

    Updated regex to look for windows paths

--- liberty-maven-plugin/src/it/include-from-bootstrap-it/src/test/java/net/wasdev/wlp/maven/test/app/IncludeBootStrapPathTest.java
+++ liberty-maven-plugin/src/it/include-from-bootstrap-it/src/test/java/net/wasdev/wlp/maven/test/app/IncludeBootStrapPathTest.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
@@ -28,16 +30,16 @@ import org.w3c.dom.NodeList;
 public class IncludeBootStrapPathTest {
 
     public final String MESSAGES_LOG = "liberty/usr/servers/test/logs/messages.log";
-    public final String INCLUDE_REGEX_MESSAGE = ".* CWWKG0028A: Processing included configuration resource: .*/target/liberty/usr/servers/test/test_resources_[123]\\.xml";
+    public final String INCLUDE_REGEX_MESSAGE = ".* CWWKG0028A: Processing included configuration resource: .*/|\\\\target/|\\\\liberty/|\\\\usr/|\\\\servers/|\\\\test/|\\\\test_resources_[123].xml";
 
     @Test
-    public void testConfigPropFileExist() throws Exception {
+    public void testMessagesLogFileExist() throws Exception {
         File f = new File(MESSAGES_LOG);
         Assert.assertTrue(f.getCanonicalFile() + " doesn't exist", f.exists());
     }
     
     @Test
-    public void testXmlElements() throws Exception {
+    public void checkMessagesLog() throws Exception {
     	File messagesLog = new File(MESSAGES_LOG);
                 
         InputStream serverOutput = null;
@@ -69,13 +71,13 @@ public class IncludeBootStrapPathTest {
         serverOutput.close();
         in.close();
 
-        Assert.assertTrue(matches.size() == 3);
+        Assert.assertTrue("Did not find all test_resources in messages.log", matches.size() == 3);
 
         String datasources_1 = matches.get(0);
         String datasources_2 = matches.get(1);
         String datasources_3 = matches.get(2);
 
-        Assert.assertTrue("test_resources wasn't resolved correctly", datasources_1.endsWith("test_resources_1.xml"));
+        Assert.assertTrue("test_resources_1 wasn't resolved correctly", datasources_1.endsWith("test_resources_1.xml"));
         Assert.assertTrue("test_resources_2 wasn't resolved correctly", datasources_2.endsWith("test_resources_2.xml"));
         Assert.assertTrue("test_resources_3 wasn't resolved correctly", datasources_3.endsWith("test_resources_3.xml"));
         
