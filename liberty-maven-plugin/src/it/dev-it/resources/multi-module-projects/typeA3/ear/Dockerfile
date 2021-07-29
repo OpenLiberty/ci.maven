6e0d110891fe8ed64ce05a04edfe05c3735fde64
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2021-07-29 11:04:50 -0400

    Track parent build files for dependency updates
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

---
+++ liberty-maven-plugin/src/it/dev-it/resources/multi-module-projects/typeA3/ear/Dockerfile
@@ -0,0 +1,8 @@
+FROM openliberty/open-liberty:kernel-java8-openj9-ubi
+
+# Add config
+COPY --chown=1001:0  target/liberty/wlp/usr/servers/defaultServer/server.xml /config/
+COPY --chown=1001:0  target/liberty/wlp/usr/servers/defaultServer/configDropins/overrides/liberty-plugin-variable-config.xml /config/configDropins/overrides/
+
+# Add application
+COPY --chown=1001:0  target/guide-maven-multimodules-ear.ear /config/apps/ 
---
+++ liberty-maven-plugin/src/it/dev-it/src/test/java/net/wasdev/wlp/test/dev/it/MultiModuleTypeA3Test.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * (c) Copyright IBM Corporation 2021.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package net.wasdev.wlp.test.dev.it;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.apache.maven.shared.utils.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MultiModuleTypeA3Test extends BaseMultiModuleTest {
+
+   @BeforeClass
+   public static void setUpBeforeClass() throws Exception {
+      setUpMultiModule("typeA3", "ear", null);
+      run();
+   }
+
+   @Test
+   public void runTest() throws Exception {
+       assertTrue(verifyLogMessageExists(
+               "The recompileDependencies parameter is set to \"true\". On a file change all dependent modules will be recompiled.",
+               20000));
+
+       File targetEarClass = new File(tempProj,
+               "ear/target/test-classes/it/io/openliberty/guides/multimodules/ConverterAppIT.class");
+       assertFalse(targetEarClass.exists());
+
+       // add a dependency to parent pom and check that it resolves compile errors in
+       // child modules
+       File parentPom = new File(tempProj, "pom.xml");
+       assertTrue(parentPom.exists());
+       replaceString("<!-- SUB JUNIT -->",
+               "<dependency> <groupId>org.junit.jupiter</groupId> <artifactId>junit-jupiter</artifactId> <version>5.6.2</version> <scope>test</scope> </dependency>",
+               parentPom);
+       Thread.sleep(5000); // wait for compilation
+       assertTrue(targetEarClass.exists());
+
+       super.manualTestsInvocation("guide-maven-multimodules-jar", "guide-maven-multimodules-war", "guide-maven-multimodules-ear");
+   }
+
+}
+
