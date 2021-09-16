c5605fae036273d84baf1d7d0a8435019e750b96
Cheryl King <cheryllk@us.ibm.com>
2021-09-16 15:59:12 -0500

    Update test case

--- liberty-maven-plugin/src/it/config-packagefile-multi-module-runnable-it/make-package/src/test/java/net/wasdev/wlp/maven/test/app/PackageRunnableJarTestIT.java
+++ liberty-maven-plugin/src/it/config-packagefile-multi-module-runnable-it/make-package/src/test/java/net/wasdev/wlp/maven/test/app/PackageRunnableJarTestIT.java
@@ -2,9 +2,7 @@ package net.wasdev.wlp.maven.test.app;
 
 import static org.junit.Assert.*;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
 
 import org.junit.Test;
 
@@ -17,17 +15,17 @@ import org.junit.Test;
 
 public class PackageRunnableJarTestIT {
 
-    private String projectBuildName = "package-runnable-jar-it-1.0-SNAPSHOT";
+    private String projectBuildName = "package-runnable-jar-it";
 
     @Test
-    public void testPackageFileJarExists() {
-        Path path = Paths.get(System.getProperty("user.dir") + "/target/" + projectBuildName + ".jar");
-        assertTrue(Files.exists(path));
+    public void testPackageFileJarExists() throws Exception {
+	File path = new File(projectBuildName + ".jar");
+        assertTrue("Package file does not exist at path: "+path.getCanonicalPath(), path.exists());
     }
     
     @Test
-    public void testPackageFileZipNotExists() {
-        Path path = Paths.get(System.getProperty("user.dir") + "/target/" + projectBuildName + ".zip");
-        assertFalse(Files.exists(path));
+    public void testPackageFileZipNotExists() throws Exception {
+	File path = new File(projectBuildName + ".zip");
+        assertFalse("Package file exists at path: "+ path.getCanonicalPath(), path.exists());
     }
 }
