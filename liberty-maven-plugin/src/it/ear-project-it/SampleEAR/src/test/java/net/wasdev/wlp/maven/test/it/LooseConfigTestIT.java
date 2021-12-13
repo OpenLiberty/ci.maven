c6d55d290f4ede206f4eeaa5eccf2a2659a0cff4
Cheryl King <cheryllk@us.ibm.com>
2021-12-13 09:33:44 -0600

    Update log4j test dependency

--- liberty-maven-plugin/src/it/ear-project-it/SampleEAR/src/test/java/net/wasdev/wlp/maven/test/it/LooseConfigTestIT.java
+++ liberty-maven-plugin/src/it/ear-project-it/SampleEAR/src/test/java/net/wasdev/wlp/maven/test/it/LooseConfigTestIT.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * (c) Copyright IBM Corporation 2017, 2019.
+ * (c) Copyright IBM Corporation 2017, 2021.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -84,9 +84,9 @@ public class LooseConfigTestIT {
         // Note: log4j-core also pulls in log4j-api
         assertEquals("Number of <archive/> element ==>", 7, nodes.getLength());
         // test runtime scope dependency to be incldued in the ?WEB-INF/lib
-        assertEquals("file targetInArchive attribute value", "/WEB-INF/lib/log4j-core-2.13.0.jar", 
+        assertEquals("file targetInArchive attribute value", "/WEB-INF/lib/log4j-core-2.15.0.jar", 
                 nodes.item(2).getAttributes().getNamedItem("targetInArchive").getNodeValue());
-        assertEquals("file targetInArchive attribute value", "/WEB-INF/lib/log4j-api-2.13.0.jar", 
+        assertEquals("file targetInArchive attribute value", "/WEB-INF/lib/log4j-api-2.15.0.jar", 
                 nodes.item(3).getAttributes().getNamedItem("targetInArchive").getNodeValue());
     }
 }
