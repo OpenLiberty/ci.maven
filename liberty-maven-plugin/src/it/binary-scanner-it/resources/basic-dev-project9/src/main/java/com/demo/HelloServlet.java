a78ffedce8cb40aa071fb0baa134b327cf81e143
Paul Gooderham <turkeyonmarblerye@gmail.com>
2022-08-15 23:06:33 -0400

    Rename scanner-it and add tests for all the EE and MP versions.
    Signed-off-by: Paul Gooderham <turkeyonmarblerye@gmail.com>

---
+++ liberty-maven-plugin/src/it/binary-scanner-it/resources/basic-dev-project9/src/main/java/com/demo/HelloServlet.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * (c) Copyright IBM Corporation 2019.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.demo;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/servlet")
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private static final Logger log = LoggerFactory.getLogger(HelloLogger.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("SLF4J Logger is ready for messages.");
+        response.getWriter().append("hello world");
+    }
+}
