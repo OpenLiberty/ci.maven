b4a79e4bd0c742b88fd93df02c5b307730389764
Evie Lau <evie@us.ibm.com>
2023-08-10 16:48:04 -0500

    Move applicationMonitor gen to StartDebugMojo:copyConfigFiles to support more goals

--- liberty-maven-plugin/src/main/java/io/openliberty/tools/maven/applications/DeployMojoSupport.java
+++ liberty-maven-plugin/src/main/java/io/openliberty/tools/maven/applications/DeployMojoSupport.java
@@ -64,7 +64,6 @@ public abstract class DeployMojoSupport extends LooseAppSupport {
     protected File copyLibsDirectory;
 
     protected ApplicationXmlDocument applicationXml = new ApplicationXmlDocument();
-    protected ApplicationMonitorConfigXmlDocument appMonXml = new ApplicationMonitorConfigXmlDocument("maven");
 
     protected void installApp(Artifact artifact) throws MojoExecutionException, IOException {
     
