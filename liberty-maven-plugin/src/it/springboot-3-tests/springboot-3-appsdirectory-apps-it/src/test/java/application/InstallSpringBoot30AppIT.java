4ffafeaecb54c31766f9c7a35dd66baae50a5e44
Arun Venmany <Arun.Kumar.V.N@ibm.com>
2024-11-18 11:19:27 +0530

    adding more tests based on review comments
    Signed-off-by: Arun Venmany <Arun.Kumar.V.N@ibm.com>

--- liberty-maven-plugin/src/it/springboot-3-tests/springboot-3-appsdirectory-apps-it/src/test/java/application/InstallSpringBoot30AppIT.java
+++ liberty-maven-plugin/src/it/springboot-3-tests/springboot-3-appsdirectory-apps-it/src/test/java/application/InstallSpringBoot30AppIT.java
@@ -27,6 +27,9 @@ public class InstallSpringBoot30AppIT {
 
         File f = new File("target/liberty/wlp/usr/servers/test/apps/thin-springboot-3-appsdirectory-apps-it-1.0.0.Final-exec.jar");
         assertTrue(f.getCanonicalFile() + " doesn't exist. Plugin failed to place the file at right destination.", f.exists());
+        File f2 = new File("target/liberty/wlp/usr/servers/test/configDropins/defaults");
+        assertTrue(f2.getCanonicalFile() + " folder doesn't exist. Plugin failed to create the config dropins folder ", f2.exists());
+        assertTrue(f2.getCanonicalFile() + " folder doesn't contain any files. Plugin failed to place config xml at right destination.", f2.list().length == 1);
     }
 
     @Test
