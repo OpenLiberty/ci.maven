4ff653e37f6950f7fba870649a8555ee9837f65d
Arun Venmany <Arun.Kumar.V.N@ibm.com>
2025-01-13 11:43:20 +0530

    passing default execution id for all tasks in execute mojo
    Signed-off-by: Arun Venmany <Arun.Kumar.V.N@ibm.com>

---
+++ liberty-maven-plugin/src/it/dev-it/src/test/java/net/wasdev/wlp/test/dev/it/DevRecompileWithCustomExecutionIdTest.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * (c) Copyright IBM Corporation 2025.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package net.wasdev.wlp.test.dev.it;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.util.Scanner;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import static io.openliberty.tools.common.plugins.util.BinaryScannerUtil.*;
+
+public class DevRecompileWithCustomExecutionIdTest extends BaseDevTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        setUpBeforeClass(null, "../resources/basic-dev-project-with-execution-id", false, false, null, null);
+    }
+
+    @AfterClass
+    public static void cleanUpAfterClass() throws Exception {
+        BaseDevTest.cleanUpAfterClass(true);
+    }
+
+    @Test
+    public void validateRunExecutionNotSkipped() throws Exception {
+        String mavenPluginCommand = "mvn compile io.openliberty.tools:liberty-maven-plugin:"+System.getProperty("mavenPluginVersion")+":dev";
+
+        StringBuilder command = new StringBuilder(mavenPluginCommand);
+        ProcessBuilder builder = buildProcess(command.toString());
+
+        builder.redirectOutput(logFile);
+        builder.redirectError(logFile);
+        process = builder.start();
+        assertTrue(process.isAlive());
+
+        OutputStream stdin = process.getOutputStream();
+
+        writer = new BufferedWriter(new OutputStreamWriter(stdin));
+
+        //java-compile is the custom execution id
+        assertTrue(getLogTail(), verifyLogMessageExists("Running maven-compiler-plugin:compile#java-compile", 120000));
+        // Check that the server has started
+        assertTrue(getLogTail(), verifyLogMessageExists("Nothing to compile - all classes are up to date.", 120000));
+    }
+}
---
+++ liberty-maven-plugin/src/it/dev-it/src/test/java/net/wasdev/wlp/test/dev/it/DevRecompileWithDefaultExecutionIdTest.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * (c) Copyright IBM Corporation 2025.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package net.wasdev.wlp.test.dev.it;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.util.Scanner;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import static io.openliberty.tools.common.plugins.util.BinaryScannerUtil.*;
+
+public class DevRecompileWithDefaultExecutionIdTest extends BaseDevTest {
+
+   @BeforeClass
+   public static void setUpBeforeClass() throws Exception {
+      setUpBeforeClass(null, "../resources/basic-dev-project", false, false, null, null);
+   }
+
+   @AfterClass
+   public static void cleanUpAfterClass() throws Exception {
+      BaseDevTest.cleanUpAfterClass(true);
+   }
+
+   @Test
+   public void validateRunExecutionNotSkipped() throws Exception {
+      String mavenPluginCommand = "mvn compile io.openliberty.tools:liberty-maven-plugin:"+System.getProperty("mavenPluginVersion")+":dev";
+
+      StringBuilder command = new StringBuilder(mavenPluginCommand);
+      ProcessBuilder builder = buildProcess(command.toString());
+
+      builder.redirectOutput(logFile);
+      builder.redirectError(logFile);
+      process = builder.start();
+      assertTrue(process.isAlive());
+
+      OutputStream stdin = process.getOutputStream();
+
+      writer = new BufferedWriter(new OutputStreamWriter(stdin));
+
+      // Check that the server has started
+      assertTrue(getLogTail(), verifyLogMessageExists("Nothing to compile - all classes are up to date.", 120000));
+   }
+}
