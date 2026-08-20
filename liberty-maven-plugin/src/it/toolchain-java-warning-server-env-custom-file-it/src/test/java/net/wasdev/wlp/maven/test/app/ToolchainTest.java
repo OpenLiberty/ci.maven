3151602ec828bbd0e1ecbd3c236e686a62f56970
Sajeer <sajeer.babu@ibm.com>
2026-08-20 14:33:36 +0530

    Updated the log assertion as per the ci.common changes https://github.com/OpenLiberty/ci.common/pull/532/changes/39339bc88740dc8906e5d0a20443e776d0aeefc6

--- liberty-maven-plugin/src/it/toolchain-java-warning-server-env-custom-file-it/src/test/java/net/wasdev/wlp/maven/test/app/ToolchainTest.java
+++ liberty-maven-plugin/src/it/toolchain-java-warning-server-env-custom-file-it/src/test/java/net/wasdev/wlp/maven/test/app/ToolchainTest.java
@@ -54,11 +54,11 @@ public class ToolchainTest {
         Assert.assertTrue(buildLog.exists());
         String os = System.getProperty("os.name");
         if (os != null && os.toLowerCase().startsWith("windows")) {
-            Assert.assertTrue("Did not find variable expansion message in build.log", logContainsMessage(buildLog, "Resolved environment variable \"EXP_VAR\" in path \"!EXP_VAR!_!EXP_VAR3!\" to \"TEST_WINDOWS\""));
-            Assert.assertTrue("Did not find second variable expansion message in build.log", logContainsMessage(buildLog, "Resolved environment variable \"EXP_VAR3\" in path \"!EXP_VAR!_!EXP_VAR3!\" to \"TEST_WINDOWS\""));
+            Assert.assertTrue("Did not find variable expansion message in build.log", logContainsMessage(buildLog, "Resolved environment variable \"EXP_VAR\" in path \"!EXP_VAR!_!EXP_VAR3!\" to \"TEST\""));
+            Assert.assertTrue("Did not find second variable expansion message in build.log", logContainsMessage(buildLog, "Resolved environment variable \"EXP_VAR3\" in path \"!EXP_VAR!_!EXP_VAR3!\" to \"WINDOWS\""));
         } else {
-            Assert.assertTrue("Did not find variable expansion message in build.log", logContainsMessage(buildLog, "Resolved environment variable \"EXP_VAR\" in path \"${EXP_VAR}_${EXP_VAR2}\" to \"TEST_UNIX\""));
-            Assert.assertTrue("Did not find second variable expansion message in build.log", logContainsMessage(buildLog, "Resolved environment variable \"EXP_VAR2\" in path \"${EXP_VAR}_${EXP_VAR2}\" to \"TEST_UNIX\""));
+            Assert.assertTrue("Did not find variable expansion message in build.log", logContainsMessage(buildLog, "Resolved environment variable \"EXP_VAR\" in path \"${EXP_VAR}_${EXP_VAR2}\" to \"TEST\""));
+            Assert.assertTrue("Did not find second variable expansion message in build.log", logContainsMessage(buildLog, "Resolved environment variable \"EXP_VAR2\" in path \"${EXP_VAR}_${EXP_VAR2}\" to \"UNIX\""));
         }
 
     }
